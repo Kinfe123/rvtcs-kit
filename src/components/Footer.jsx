37e86ec1aef694cf102a31f4cb6b5f72dbae9380
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-11-10 17:24:14 +0300

    Update Footer.jsx
    fix: footer naming issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © {new Date().getFullYear()}{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <a href="https://kinfish-owns-computer.vercel.app/" className="hover:underline">
            RVTCS™ - KINFEMICHAEL TARIKU - KiNFiSH 
           </a>
           . All Rights Reserved.
